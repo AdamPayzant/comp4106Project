cd2038f1dbebd61afe6aca9a3291f0dbf04b6702
AdamPayzant <payzantedwardiv@gmail.com>
2021-03-22 17:57:52 -0400

    Started making the game

---
+++ src/Settlers/settlers.py
@@ -0,0 +1,8 @@
+class Game:
+    nodes = []
+
+    def __init__(self):
+        pass
+    
+    def genBoard(self):
+        pass
---
+++ src/Settlers/structures.py
@@ -0,0 +1,29 @@
+class Map:
+    map = []
+    roads = []
+    settlements = []
+
+    def __init__(self):
+        pass
+
+    def __genMap(self):
+        pass
+
+class Tile:
+    def __init__(self, res: str, roll: int, index:int):
+        self.res = res
+        self.roll = roll
+        self.index = index
+        self.adj = {}
+
+class Graph:
+    def __init__(self):
+        pass
+
+class Node:
+    def __init__(self):
+        pass
+
+class Vertix:
+    def __init__(self):
+        pass
